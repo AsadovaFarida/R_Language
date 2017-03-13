dff599328da027c9661a4eeba2cc8e0540882b8d
AsadovaFarida <asadovafarida@gmail.com>
2017-03-13 21:45:56 +0400

    Finite element method

--- task1_solution.R
+++ task1_solution.R
@@ -1,13 +1,4 @@
-#function M = MassAssembler1D(x)
-#n = length(x)-1; % number of subintervals
-#M = zeros(n+1,n+1); % allocate mass matrix
-#for i = 1:n % loop over subintervals
-#h = x(i+1) - x(i); % interval length
-#M(i,i) = M(i,i) + h/3; % add h/3 to M(i,i)
-#M(i,i+1) = M(i,i+1) + h/6;
-#M(i+1,i) = M(i+1,i) + h/6;
-#M(i+1,i+1) = M(i+1,i+1) + h/3;
-#end
+#page 17-Assembly of the mass matrix
 
 MassAssembler1D<-function(x){
 n<-length(x)-1
@@ -17,10 +8,49 @@ for(i in 1:n) {
   h<-x.num[i+1]-x.num[i]
   M[i,i]=M[i,i]+h/3
   M[i,i+1]=M[i,i+1]+h/6
-  M[i+1,i]= M[i+1,i]+h/6;
+  M[i+1,i]= M[i+1,i]+h/6
   M[i+1,i+1]=M[i+1,i+1]+h/3
 }
-  Mylist<-print(list(M))
+Mass<-print(as.matrix(M))
 }
 
-u<-MassAssembler1D(list(1,2,3,4,5))
+#page 19-Assembly of the load vector
+
+LoadAssembler1D<-function(x,f){
+n<-length(x)-1
+b<-matrix(0,n+1,1)
+x.num<-as.numeric(x)
+for(i in 1:n) {
+  h<-x.num[i+1]-x.num[i]
+  b[i]=b[i] + f(x.num[i])*h/2
+  b[i+1]= b[i+1]+ f(x.num[i+1])*h/2
+}
+  Vector<-print(as.vector(b))
+}
+
+#Fuction creation
+f<-function(x){
+x.num<-as.numeric(x)
+y=x.num*sin(x.num)
+}
+
+#page 20- L2Projection
+
+L2Projector1D<-function(){
+n=5
+h<-1/n
+x=list(0,h,1)
+M<-MassAssembler1D(x)
+b<-LoadAssembler1D(x,f)
+print(is.numeric(M))
+print(is.numeric(b))
+Pf=solve(M,b)
+print(Pf)
+plot(x,Pf) 
+lines(x,Pf)
+}
+
+u<-L2Projector1D()
+
+
+
